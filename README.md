c78316e79ecd89c861e632cbbec34a4f74f5615f
Edi <edi.sinovcic@gmail.com>
2020-04-07 23:34:31 +0200

    Adding basic readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Solc version manager
+
+This is a simple solc version downloader
+
+It's a simple tool that check if version exists locally, if it doesn't it downloads version from remote.
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process');
 
-console.log(process.argv)
 if(process.argv.length < 3){
-    console.log("By default we are fetching latest version for you. \nIf you want a specific version please input it as an argument.")
+    console.log("By default we are fetching latest version for you. \nIf you want a specific version please input it as an argument.\nFor example: solc-version-manager v0.4.26+commit.4563c3fc")
 }
 
 const version = process.argv[2] || "latest";
@@ -16,6 +15,7 @@ function fetchRemote(version) {
     try {
         console.log("Fetching")
         execSync(`curl -o /tmp/${version}.js https://ethereum.github.io/solc-bin/bin/soljson-${version}.js`);
+        console.log(`Version ${version} has been downloaded from ${url} and it's located in ${path}`);
         return path;
     } catch(error) {
         console.log(`Failed to fetch version: ${version} from url ${url}. Please check that you inputed correct version and that version exists!`)
